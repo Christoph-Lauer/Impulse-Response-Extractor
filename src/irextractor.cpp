6b58e765571027c8e97d8c059e3286e270bf5433
Christoph Lauer <78355888+Christoph-Lauer@users.noreply.github.com>
2021-09-26 07:48:55 +0200

    Add files via upload

--- src/irextractor.cpp
+++ src/irextractor.cpp
@@ -5,6 +5,7 @@
 #include <QtMultimedia>
 
 #include <iostream>
+#include <cstdlib>
 
 IRExtractor::IRExtractor(QWidget *parent)
     : QMainWindow(parent)
@@ -18,7 +19,7 @@ IRExtractor::~IRExtractor()
     delete ui;
 }
 
-
+QAudioDecoder *decoder = new QAudioDecoder();
 // NOTE: This code should play and recors simultaneously but I didn't get sucess.
 // The player plays for his own and the recoder records for his own but i cannot duplex both.
 // I tried timers, maked the members static, used threads and connect all the
@@ -27,12 +28,21 @@ IRExtractor::~IRExtractor()
 void IRExtractor::on_sweepButton_clicked()
 {
     // PLAY
-    QMediaPlayer* player = new QMediaPlayer();
-    QAudioOutput* audioOutput = new QAudioOutput;
-    audioOutput->setVolume(0.1);
-    player->setAudioOutput(audioOutput);
-    player->setSource(QUrl("qrc:/res/sweep.wav"));
-    player->play();
+// DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEB
+// EBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBU
+// BUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+//    QMediaPlayer* player = new QMediaPlayer();
+//    QAudioOutput* audioOutput = new QAudioOutput;
+//    audioOutput->setVolume(0.1);
+//    player->setAudioOutput(audioOutput);
+//    player->setSource(QUrl("qrc:/res/sweep.wav"));
+//    player->play();
+// DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEB
+// EBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBU
+// BUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
+
+    system("/usr/bin/afplay /Users/christoph/Documents/DEVELOPMENT/Impulse-Response-Extractor/src/res/sweep.wav &");
+    system("rm ~/Music/test.wav");
 
     // RECORD
     QMediaCaptureSession* session = new QMediaCaptureSession();
@@ -48,4 +58,29 @@ void IRExtractor::on_sweepButton_clicked()
     QThread::msleep(3250);
     recorder->stop();
 
+    // READ
+    QAudioFormat desiredFormat;
+    desiredFormat.setChannelCount(2);
+    desiredFormat.setSampleFormat(QAudioFormat::Int16);
+    desiredFormat.setSampleRate(48000);
+    decoder->setAudioFormat(desiredFormat);
+    decoder->setSource(QUrl::fromLocalFile("/Users/christoph/Music/test.wav"));
+    connect(decoder, SIGNAL(bufferReady()), this, SLOT(readBuffer()));
+    decoder->start();
+}
+
+QVector<int> samples;
+void IRExtractor::readBuffer()
+{
+    qDebug() << "READ BUFFER";
+    QAudioBuffer buffer = decoder->read();
+    qDebug() << buffer.format();
+    qDebug() << buffer.sampleCount();
+    qDebug() << buffer.frameCount();
+    quint16 *data = buffer.data<quint16>();
+    for(int i=0; i<buffer.sampleCount(); i++)
+    {
+        samples.push_back(data[i]);
+    }
+    qDebug() << samples.size();
 }
--- src/irextractor.h
+++ src/irextractor.h
@@ -18,6 +18,7 @@ public:
 
 private slots:
     void on_sweepButton_clicked();
+    void readBuffer();
 
 private:
     Ui::IRExtractor *ui;
